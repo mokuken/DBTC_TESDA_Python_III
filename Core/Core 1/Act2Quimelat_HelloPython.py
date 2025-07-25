15deb4d04c71e26b20c38abb82e9b3c032a8372e
mokuken <imsilencer44@gmail.com>
2025-07-25 15:46:17 +0800

    added activity 2

---
+++ Core/Core 1/Act2Quimelat_HelloPython.py
@@ -0,0 +1,5 @@
+# Input the user information/data and then output the data
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello,", name + "!")
+print("You are", age, "years old.")

789beb7d23b38ddade101d3430744cef6413e4f9
mokuken <imsilencer44@gmail.com>
2025-07-25 15:46:25 +0800

    added activity 3

---
+++ Core/Core 1/Act3Quimelat_PersonalInfo.py
@@ -0,0 +1,13 @@
+# Prompt the user to input the following details
+full_name = input("Enter your full name: ")
+age = input("Enter your age: ")
+course = input("Enter your course: ")
+school_name = input("Enter your school name: ")
+favorite_programming_language = input("Enter your favorite programming languge: ")
+
+# Display the information back in a formatted and readable way
+print("Full Name:", full_name)
+print("Age:", age)
+print("Course:", course)
+print("School Name:", school_name)
+print("Favorite Programming Language:", favorite_programming_language)

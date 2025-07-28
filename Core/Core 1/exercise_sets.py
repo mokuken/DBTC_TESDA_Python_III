cf8eec3fd8b72911f6de209a6617c5cf2da3d817
mokuken <imsilencer44@gmail.com>
2025-07-28 14:58:40 +0800

    added exercise sets

---
+++ Core/Core 1/exercise_sets.py
@@ -0,0 +1,8 @@
+# list of names with repeated names
+names = ["harly", "kenji", "jessie", "ryan", "kenji", "harly"]
+
+# convert the list to set
+names_tuple = set(names)
+
+# print the converted list
+print(names_tuple)

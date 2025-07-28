935605997d1c4b9581b3058a39d863b65b83b274
mokuken <imsilencer44@gmail.com>
2025-07-28 15:00:03 +0800

    test playground changed

--- Core/Core 1/test.py
+++ Core/Core 1/test.py
@@ -1,11 +1,2 @@
-fruits = ("apple", "banana", "cherry")
-
-print(fruits)
-
-converted = list(fruits)
-
-converted.append("orange")
-
-fruits = tuple(converted)
-
-print(fruits)
+names = {"box","apple", "cat", "apple"}
+print(sorted(names))
